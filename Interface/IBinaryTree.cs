b5d5da5864223e27a30584c4d103552ed5db3998
Suraysen Thiyagarasu <suraysen@yahoo.com>
2024-04-18 12:33:48 +0800

    Add project files.

---
+++ Interface/IBinaryTree.cs
@@ -0,0 +1,29 @@
+﻿using BinaryTree.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree.Interface
+{
+    public interface IBinaryTree
+    {
+        public bool Add(int value);
+        public Node Find(int value);
+        public void Remove(int value);
+        public Node Remove(Node parent, int key);
+        public int MinValue(Node node);
+
+        public Node Find(int value, Node parent);
+        public int GetTreeDepth();
+        public int GetTreeDepth(Node parent);
+
+        public void TraversePreOrder(Node parent);
+        public void TraversePostOrder(Node parent);
+
+        public void TraverseInOrder(Node parent);
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BinaryTree.Model;
+
+Console.WriteLine("Start!");
+BinaryTree.Class.BinaryTree binaryTree = new BinaryTree.Class.BinaryTree();
+
+int[] input = { 1, 2, 7, 3, 10, 5, 8 };
+var output = String.Empty;
+foreach (var itm in input)
+{
+    binaryTree.Add(itm);
+    output += "," + itm;
+}
+Console.WriteLine("Input :" + output);
+
+
+Node node = binaryTree.Find(5);
+int depth = binaryTree.GetTreeDepth();
+
+Console.WriteLine("PreOrder Traversal:");
+binaryTree.TraversePreOrder(binaryTree.Root);
+Console.WriteLine();
+
+Console.WriteLine("InOrder Traversal:");
+binaryTree.TraverseInOrder(binaryTree.Root);
+Console.WriteLine();
+
+Console.WriteLine("PostOrder Traversal:");
+binaryTree.TraversePostOrder(binaryTree.Root);
+Console.WriteLine();
